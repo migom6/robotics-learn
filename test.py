ed6465f23ea2dc712dcd268e859530ac395a7751
Madhurjya <migom6@gmail.com>
2018-09-26 19:54:30 +0530

    test cases added

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from isOrthogonal import *
 from arctan import *
 
 # assignment 1
+# def inverse(matrix): return matrix
 
 m = [[-0.7078, 0.2855, 0.6461, 3],[-0.5806, 0.2858, -0.7824, 3], [-0.4023, -0.9140, -0.0365, 3], [0,0,0,1]]
 
@@ -11,5 +12,12 @@ n = inverse(m)
 print(isOrthogonal(m,n))
 
 #assignment 2 
-
 # def arctan(y,x): return degrees
+
+print(arctan(1,1))
+print(arctan(1,-1))
+print(arctan(-1,1))
+print(arctan(-1,-1))
+print(arctan(0,-1))
+print(arctan(1,0))
+
